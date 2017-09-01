09040741764a8e58ec3d0997eba94fea1ffa02b2
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2017-09-01 14:19:22 +0530

    postive.c

---
+++ postive.c
@@ -0,0 +1,15 @@
+# set
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the n value");
+scanf("%d",&n);
+if(n>0)
+{
+printf("enter the postive number");
+}
+else
+printf("enter the negative  number");
+}
+}}
