8f39c590e1e9a77ec4ca96d95085436baeaa088d
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2017-09-01 14:35:05 +0530

    even.c

---
+++ even.c
@@ -0,0 +1,17 @@
+# set
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the nuber");
+scanf("%d",&n);
+if(n%==2)
+{
+printf("the given number is even");
+}
+else
+{
+printf(" the given number is odd");
+}
+}
+
